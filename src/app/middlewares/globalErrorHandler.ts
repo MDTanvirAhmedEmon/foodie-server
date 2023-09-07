519ebfbb60669a959acfb8a47f91761f0e2886e6
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2023-09-07 23:03:12 +0600

    finish error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -5,6 +5,7 @@ import {
   IGenericErrorResponse,
 } from '../../interfaces/error'
 import handelValidationError from '../../errors/handleValidationError'
+import ApiError from '../../errors/ApiError'
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   // res.status(400).json({emon: error});
@@ -18,6 +19,27 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
   }
 
   res.status(statusCode).json({
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,43 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+import { Server } from 'http'
+
+process.on('uncaughtException', error => {
+  console.log('uncaughtException is detected', error)
+  process.exit(1)
+})
+
+let server: Server
 
 async function main() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log('Database is connected successfully');
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-          })
-    }catch(error) {
-        console.log(`Failed to connect to database`)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is connected successfully')
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`Failed to connect to database`)
+  }
 
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        console.log(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
-main();
+main()
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM is received')
+  if (server) {
+    server.close()
+  }
+})
