a9ccf87010d1b40a3aedd72620edec2f48ba5351
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2023-10-28 20:02:59 +0600

    Modified products routes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { managedRouter } from './app/routes'
+import { productRoutes } from './app/modules/products/products.route'
+
 const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/v1', managedRouter)
+app.use('/api/v1/products', productRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
